224e24a067c9d7146b83bd450e72ffb5016c9e75
samir cointab <cyrilekta@gmail.com>
2022-12-07 22:55:51 +0530

    changes for deployment

--- nextlab/settings.py
+++ nextlab/settings.py
@@ -21,12 +21,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-7@wd!g9i4xxre5*rx5*_*p)x)i_5c5058wj+4$0dp^ms1jhjqx'
+SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = config('DEBUG', cast=bool)
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['nextlabtest-production.up.railway.app']
 
 
 # Application definition
